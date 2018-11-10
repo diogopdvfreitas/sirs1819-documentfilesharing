151c92d5a8a5d4d3806468feb8724580b52741d3
Henrique Santos <josehenriquesantos@tecnico.ulisboa.pt>
2018-11-10 16:11:29 +0000

    Remove creation of Keymanager on main function

--- ca/src/main/java/a47/ca/CaApplication.java
+++ ca/src/main/java/a47/ca/CaApplication.java
@@ -1,6 +1,5 @@
 package a47.ca;
 
-import a47.ca.keyManager.KeyManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CaApplication {
 
 	public static void main(String[] args) {
-		KeyManager keyManager;
 		SpringApplication.run(CaApplication.class, args);
 	}
 }
