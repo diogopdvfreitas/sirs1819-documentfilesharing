8b64b09a8762d9efae9458a67f5c3fe79f676f3e
Henrique Santos <josehenriquesantos@tecnico.ulisboa.pt>
2018-11-10 16:14:24 +0000

    Update RequestPubKey to Request requirements

--- ca/src/main/java/a47/ca/model/RequestPubKey.java
+++ ca/src/main/java/a47/ca/model/RequestPubKey.java
@@ -2,23 +2,21 @@ package a47.ca.model;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.security.PublicKey;
 
-public class PublishPubKey {
+public class RequestPubKey {
     @NotNull
     @NotBlank
     private String username;
 
     @NotNull
     @NotBlank
-    private PublicKey publicKey;
+    private String usernameToGetPubKey;
 
     public String getUsername() {
         return username;
     }
 
-    public PublicKey getPublicKey() {
-        return publicKey;
+    public String getUsernameToGetPubKey() {
+        return usernameToGetPubKey;
     }
-
 }
