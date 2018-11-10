d71200c9c7bd5a1f675470b882594bead104c686
Henrique Santos <josehenriquesantos@tecnico.ulisboa.pt>
2018-11-10 23:31:19 +0000

    Add decodePubKey method

--- ca/src/main/java/a47/ca/keyManager/AuxMethods.java
+++ ca/src/main/java/a47/ca/keyManager/AuxMethods.java
@@ -4,6 +4,11 @@ import a47.ca.Constants;
 
 import javax.crypto.Cipher;
 import java.security.Key;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 
 public class AuxMethods {
     public static byte[] cipherWithKey(byte[] data, Key key) throws Exception {
@@ -16,5 +21,10 @@ public class AuxMethods {
         } catch (Exception e) {
             throw new Exception();
         }
+    }
+    public static PublicKey decodePubKey(byte[] encodedPubKey) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        X509EncodedKeySpec ks = new X509EncodedKeySpec(encodedPubKey);
+        KeyFactory kf = KeyFactory.getInstance(Constants.Keys.CA_KEYSTORE_CIPHER);
+        return kf.generatePublic(ks);
     }
 }
