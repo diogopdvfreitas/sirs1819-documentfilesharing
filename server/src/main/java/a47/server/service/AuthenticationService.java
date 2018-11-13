3ccea1bbc97ea616fec9e6b78b05db81825b345a
Joao Silvestre <joao.p.silvestre@tecnico.ulisboa.pt>
2018-11-13 01:10:08 +0000

    Server stores users and password on a txt file

--- server/src/main/java/a47/server/service/AuthenticationService.java
+++ server/src/main/java/a47/server/service/AuthenticationService.java
@@ -3,9 +3,67 @@ package a47.server.service;
 import a47.server.model.RegisterUser;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.io.*;
+import java.util.HashMap;
+
 @Service
 public class AuthenticationService {
-    public boolean registerUser(RegisterUser registerUser){// dummy register
+
+    private File usersFile = new File("users.txt");
+
+    @PostConstruct
+    public void init() {
+        try {
+            usersFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public boolean registerUser(RegisterUser registerUser){
+        String users = readFile("users.txt");
+        HashMap<String, String> usersHash = getUsers(users);
+        if(usersHash.containsKey(registerUser.getUsername()))
+            return false;
+        writeToFile("users.txt", registerUser.getUsername() + " " + registerUser.getPassword());
         return true;
+    }
+
+    private void writeToFile(String fileName, String text){
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, true));
+            bufferedWriter.write(text);
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readFile(String fileName){
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            while((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+
+    private HashMap<String,String> getUsers(String users){
+        HashMap<String,String> hashMap = new HashMap<>();
+        String[] split = users.split("\n");
+        for (String line: split) {
+            String pair[] = line.split(" ");
+            if(pair.length == 1)
+                break;
+            hashMap.put(pair[0], pair[1]);
+        }
+        return hashMap;
     }
 }
