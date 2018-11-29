9279acc3049f050687cab0ca049be294136113f4
Henrique Santos <josehenriquesantos@tecnico.ulisboa.pt>
2018-11-29 11:51:13 +0000

    Add logger to AuthenticationController

--- server/src/main/java/a47/server/controller/AuthenticationController.java
+++ server/src/main/java/a47/server/controller/AuthenticationController.java
@@ -2,6 +2,7 @@ package a47.server.controller;
 
 import a47.server.model.RegisterUser;
 import a47.server.service.AuthenticationService;
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("auth")
 public class AuthenticationController {
-
+    private static Logger logger = Logger.getLogger(AuthenticationController.class);
     private AuthenticationService authenticationService;
 
     @Autowired
@@ -24,7 +25,9 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterUser registerUser){
+        logger.info("Registering user " + registerUser.getUsername());
         authenticationService.registerUser(registerUser);
+        logger.info("Registration user " + registerUser.getUsername()+" done");
         return ResponseEntity.ok("User registered with success");
     }
 
