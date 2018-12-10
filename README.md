be22b4c01b908c9a4f2df7811414b02482c0fe42
João Silvestre <joao.p.silvestre@tecnico.ulisboa.pt>
2018-12-10 18:40:26 +0000

    server and ca serving https update

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Development Enviroment:\
 Java 11.0.1(LTS)\
 IntelliJ IDEA 2018.2.6 (fyi)\
 Maven 3.6.0
+
+## Certificate 
+To import certificate to jre use:\
+$ keytool -importcert -file serverCertificate.crt -alias server -keystore $JDK_HOME/jre/lib/security/cacerts
+$ keytool -importcert -file caCertificate.crt -alias ca -keystore $JDK_HOME/jre/lib/security/cacerts
--- gencert.sh
+++ gencert.sh
@@ -7,47 +7,69 @@ CA_NAME=ca
 SERVER_NAME=server
 STOREPASS="password"
 
+generate_certificate_chain(){
+    echo "Generating ca root keystore"
+    keytool -storepass ${STOREPASS} -genkeypair -storetype pkcs12 -keystore ${ROOT_NAME}.p12 -alias ${ROOT_NAME} -ext bc:c -dname "CN=localhost, OU=SIRS, O=IST, L=Lisbon, S=Lisbon, C=PT"
+    echo "Generating ca keystore"
+    keytool -storepass ${STOREPASS} -genkeypair -storetype pkcs12 -keystore ${CA_NAME}.p12 -alias ${CA_NAME} -ext bc:c -dname "CN=localhost, OU=SIRS, O=IST, L=Lisbon, S=Lisbon, C=PT"
+    echo "Generating server keystore"
+    keytool -storepass ${STOREPASS} -genkeypair -storetype pkcs12 -keystore ${SERVER_NAME}.p12 -alias ${SERVER_NAME} -dname "CN=localhost, OU=SIRS, O=IST, L=Lisbon, S=Lisbon, C=PT"
 
-generate_root_ca() {
-    echo "Generating ca root key"
-    openssl genrsa -out ${ROOT_NAME}.key 2048
-    echo "Creating root ca certificate"
-    openssl req -new -x509 -days 1826 -key ${ROOT_NAME}.key -out ${ROOT_NAME}.crt -subj ${SUBJ}
+    echo "Exporting ca root certificate"
+    keytool -storepass ${STOREPASS} -keystore ${ROOT_NAME}.p12 -alias ${ROOT_NAME} -exportcert -rfc > ${ROOT_NAME}.pem
+
+    echo "Generating ca certificate"
+    keytool -storepass ${STOREPASS} -keystore ${CA_NAME}.p12 -certreq -alias ${CA_NAME} | keytool -storepass ${STOREPASS} -keystore ${ROOT_NAME}.p12 -gencert -alias ${ROOT_NAME} -ext BC=0 -rfc > ${CA_NAME}.pem
+    echo "Signing ca certificate with ca root"
+    cat ${ROOT_NAME}.pem ${CA_NAME}.pem > cachain.pem
+    keytool -storepass ${STOREPASS} -keystore ${CA_NAME}.p12 -importcert -alias ${CA_NAME} -file cachain.pem
+
+    echo "Generating server certificate"${CA_NAME}
+    keytool -storepass ${STOREPASS} -keystore ${SERVER_NAME}.p12 -certreq -alias ${SERVER_NAME} | keytool -storepass ${STOREPASS} -keystore ${CA_NAME}.p12 -gencert -alias ${CA_NAME} -ext ku:c=dig,keyEncipherment -rfc > ${SERVER_NAME}.pem
+    echo "Signing server certificate with ca root"
+    cat ${ROOT_NAME}.pem ${CA_NAME}.pem ${SERVER_NAME}.pem > serverchain.pem
+    keytool -storepass ${STOREPASS} -keystore ${SERVER_NAME}.p12 -importcert -alias ${SERVER_NAME} -file serverchain.pem
 }
 
-generate_ca(){
-    echo "Generating ca key"
-    keytool -storepass ${STOREPASS} -genkey -alias ${CA_NAME} -keyalg RSA -storetype pkcs12 -keystore ${CA_NAME}.p12 -keysize 2048 -dname "CN=localhost, OU=SIRS, O=IST, L=Lisbon, S=Lisbon, C=PT"
-    echo "Generating server csr"
-    keytool -storepass ${STOREPASS} -certreq -alias ${CA_NAME} -storetype pkcs12 -keystore ${CA_NAME}.p12 -file ${CA_NAME}.csr
-    echo "Signing server with root ca"
-    openssl x509 -req -days 1000 -in ${CA_NAME}.csr -CA ${ROOT_NAME}.crt -CAkey ${ROOT_NAME}.key -CAcreateserial -out ${CA_NAME}.crt
 
-    keytool -import -trustcacerts -alias ${CA_NAME} -file ${CA_NAME}.crt -storetype pkcs12 -keystore ${CA_NAME}.p12
-
-    openssl x509 -text -noout -in ${CA_NAME}.crt
-}
-
-generate_server(){
-    echo "Generating server key"
-    keytool -storepass ${STOREPASS} -genkey -alias ${SERVER_NAME} -keyalg RSA -storetype pkcs12 -keystore ${SERVER_NAME}.p12 -keysize 2048 -dname "CN=localhost, OU=SIRS, O=IST, L=Lisbon, S=Lisbon, C=PT"
-    echo "Generating server csr"
-    keytool -storepass ${STOREPASS} -certreq -alias ${SERVER_NAME} -storetype pkcs12 -keystore ${SERVER_NAME}.p12 -file ${SERVER_NAME}.csr
-    echo "Signing server with root ca"
-    openssl x509 -req -days 1000 -in ${SERVER_NAME}.csr -CA ${ROOT_NAME}.crt -CAkey ${ROOT_NAME}.key -CAcreateserial -out ${SERVER_NAME}.crt
-
-    keytool -import -trustcacerts -alias ${SERVER_NAME} -file ${SERVER_NAME}.crt -storetype pkcs12 -keystore ${SERVER_NAME}.p12
-
-    openssl x509 -text -noout -in ${SERVER_NAME}.crt
-}
-
-import_certificate(){
-    echo "Importing server certificate to keystore"
-    #keytool -import -alias server -file server.crt -keystore keystore.p12 -storepass password
-    keytool -import -trustcacerts -alias ${SERVER_NAME} -file ${SERVER_NAME}.crt -keystore ${SERVER_NAME}.jks
-}
-
-#To import certificate to jre use: $ keytool -importcert -file serverCertificate.crt -alias server -keystore $JDK_HOME/jre/lib/security/cacerts
+#generate_root_ca() {
+#    echo "Generating ca root key"
+#    openssl genrsa -out ${ROOT_NAME}.key 2048
+#    echo "Creating root ca certificate"
+#    openssl req -new -x509 -days 1826 -key ${ROOT_NAME}.key -out ${ROOT_NAME}.crt -subj ${SUBJ}
+#}
+#
+#generate_ca(){
+#    echo "Generating ca key"
+#    keytool -storepass ${STOREPASS} -genkey -alias ${CA_NAME} -keyalg RSA -storetype pkcs12 -keystore ${CA_NAME}.p12 -keysize 2048 -dname "CN=localhost, OU=SIRS, O=IST, L=Lisbon, S=Lisbon, C=PT"
+#    echo "Generating server csr"
+#    keytool -storepass ${STOREPASS} -certreq -alias ${CA_NAME} -storetype pkcs12 -keystore ${CA_NAME}.p12 -file ${CA_NAME}.csr
+#    echo "Signing server with root ca"
+#    openssl x509 -req -days 1000 -in ${CA_NAME}.csr -CA ${ROOT_NAME}.crt -CAkey ${ROOT_NAME}.key -CAcreateserial -out ${CA_NAME}.crt
+#
+#    keytool -import -trustcacerts -alias ${CA_NAME} -file ${CA_NAME}.crt -storetype pkcs12 -keystore ${CA_NAME}.p12
+#
+#    openssl x509 -text -noout -in ${CA_NAME}.crt
+#}
+#
+#generate_server(){
+#    echo "Generating server key"
+#    keytool -storepass ${STOREPASS} -genkey -alias ${SERVER_NAME} -keyalg RSA -storetype pkcs12 -keystore ${SERVER_NAME}.p12 -keysize 2048 -dname "CN=localhost, OU=SIRS, O=IST, L=Lisbon, S=Lisbon, C=PT"
+#    echo "Generating server csr"
+#    keytool -storepass ${STOREPASS} -certreq -alias ${SERVER_NAME} -storetype pkcs12 -keystore ${SERVER_NAME}.p12 -file ${SERVER_NAME}.csr
+#    echo "Signing server with root ca"
+#    openssl x509 -req -days 1000 -in ${SERVER_NAME}.csr -CA ${ROOT_NAME}.crt -CAkey ${ROOT_NAME}.key -CAcreateserial -out ${SERVER_NAME}.crt
+#
+#    keytool -import -trustcacerts -alias ${SERVER_NAME} -file ${SERVER_NAME}.crt -storetype pkcs12 -keystore ${SERVER_NAME}.p12
+#
+#    openssl x509 -text -noout -in ${SERVER_NAME}.crt
+#}
+#
+#import_certificate(){
+#    echo "Importing server certificate to keystore"
+#    #keytool -import -alias server -file server.crt -keystore keystore.p12 -storepass password
+#    keytool -import -trustcacerts -alias ${SERVER_NAME} -file ${SERVER_NAME}.crt -keystore ${SERVER_NAME}.jks
+#}
 
 generate_certificate_server(){
     echo "Generating client server certificate"
@@ -65,20 +87,14 @@ if [[ -z "$1" ]]
     exit
 fi
 
-if test $1 = root; then
-    generate_root_ca
-elif test $1 = ca; then
-    generate_ca
-elif test $1 = server; then
-    generate_server
+if test $1 = chain; then
+    generate_certificate_chain
 elif test $1 = serverCert; then
     generate_certificate_server
 elif test $1 = caCert; then
     generate_certificate_ca
 elif test $1 = all; then
-    generate_root_ca
-    generate_ca
-    generate_server
+    generate_certificate_chain
     generate_certificate_server
     generate_certificate_ca
 else
